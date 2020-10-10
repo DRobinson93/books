8590f6fe5b7c3798d2e0fa212709aee56604504d
drobinson93 <danpaulrob1@gmail.com>
2020-10-10 15:45:15 -0400

    fix tailwind config to get correct classes for live

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/components/*.vue',
+        './resources/js/components/**/*.vue',
     ],
 
     theme: {
